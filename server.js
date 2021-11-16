5f11c227f637cdb1846baa561ca50cb206ba872b
Yannick-Beau <beauyannick@gmail.com>
2021-11-16 16:07:58 +0100

    integration ok

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const app = require('express')();
+const { request, response } = require('express');
+const express = require('express');
+const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/assets', express.static('public'));
+
 app.get('/', (request, response) => {
     response.render('pages/index', {
-        test: 'Salut',
+        test: 'Salut !',
     });
+});
+app.post('/', (request, response) => {
+    console.log(request.body);
 });
 
 app.listen(8080);
